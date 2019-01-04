dcfce24f6155c79eba8a6d3bfa129977941a6468
hassan adel <hasanadel555@hotmail.com>
2019-01-04 04:23:57 +0200

    Implement UserExists, Login and VerifyPasswordHash

--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -9,7 +9,7 @@ namespace SocialApp.API.Data
     public interface IAuthRepository
     {
         Task<User> Register(User user, string password);
-        Task<User> Login(User user, string password);
+        Task<User> Login(string username, string password);
         Task<bool> UserExists(string username);
     }
 }
