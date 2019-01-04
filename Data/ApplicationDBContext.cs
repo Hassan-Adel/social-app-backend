aa9eefcae7fea4e077a8cb49655998df4bd8b336
hassan adel <hasanadel555@hotmail.com>
2019-01-04 03:31:13 +0200

    Create Application DB context

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base (options) { }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
