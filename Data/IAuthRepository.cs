c0f188b17d43b9fb9cdf8292bbf10fb1efb1079c
hassan adel <hasanadel555@hotmail.com>
2019-01-04 03:38:26 +0200

    Add IAuthRepository

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(User user, string password);
+        Task<bool> UserExists(string username);
+    }
+}
