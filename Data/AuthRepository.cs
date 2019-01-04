5f145b310e4a4ce97f2a67446b09d04f5ad1727b
hassan adel <hasanadel555@hotmail.com>
2019-01-04 03:39:02 +0200

    Create AuthRepository implementing IAuthRepository

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private ApplicationDBContext _context;
+        public AuthRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public Task<User> Login(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
