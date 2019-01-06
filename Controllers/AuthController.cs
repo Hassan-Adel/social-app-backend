9ddf48a86f4f483de725408463c71a92f5b98155
hassan adel <hasanadel555@hotmail.com>
2019-01-06 03:26:07 +0200

    Add AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SocialApp.API.Data;
+using SocialApp.API.DTOs;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        // GET api/values
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDTO userForRegisterDTO)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDTO.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User { Username = userForRegisterDTO.Username };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
+            
+            return StatusCode(201);
+        }
+
+        // we don't need [FromBody] as it's already being added by the [ApiController]
+        // GET api/values
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserForLoginDTO userForLoginDTO)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var userFromRepo = await _repo.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var toKenHandler = new JwtSecurityTokenHandler();
+
+            var token = toKenHandler.CreateToken(tokenDescriptor);
+            return Ok(new {
+                token = toKenHandler.WriteToken(token)
+            });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            
+            return Ok();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
