87bd8fa33701ae0577a153eaec5d24eb9d36d63c
hassan adel <hasanadel555@hotmail.com>
2019-01-06 03:27:36 +0200

    Validating UserForRegister using DataAnnotations

--- DTOs/UserForRegisterDTO.cs
+++ DTOs/UserForRegisterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace SocialApp.API.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Password should be between 4 and 10 characters.")]
         public string Password { get; set; }
     }
 }
