e0d9b27cbe5cbd30964e23583d9cedbc68f17f44
hassan adel <hasanadel555@hotmail.com>
2019-01-11 05:44:35 +0200

    Create interface ISocialRepository

---
+++ Data/ISocialRepository.cs
@@ -0,0 +1,15 @@
+﻿using SocialApp.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Data
+{
+    public interface ISocialRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser();
+    }
+}
