eedd8363ab591dd363c0e582f177d99a7ffcaeac
hassan adel <hasanadel555@hotmail.com>
2019-01-12 16:39:30 +0200

    Adding AutoMapperProfiles

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SocialApp.API.DTOs;
+using SocialApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap <User, UserForDetailedDTO>();
+            CreateMap<User, UserForListDTO>();
+        }
+    }
+}
