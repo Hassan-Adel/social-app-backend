fbdf02962de68e7a00efc0bca098e901e4f7fbd5
hassan adel <hasanadel555@hotmail.com>
2019-01-12 05:50:32 +0200

    Add UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialApp.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ISocialRepository _repo;
+        public UsersController(ISocialRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            return Ok(users);
+        }
+
+        // GET api/user/id
+        [HttpGet]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            return Ok(user);
+        }
+
+    }
+}
