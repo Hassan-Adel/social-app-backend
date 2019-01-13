ab225190a0747bd6f32c885cf001b8e7129e3c22
hassan adel <hasanadel555@hotmail.com>
2019-01-13 02:54:09 +0200

    Adding CalculateAge as DateTime extension

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -16,5 +16,24 @@ namespace SocialApp.API.Helpers
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
+        /*
+         *  But this isn't going to give us an accurate age because depending on the time of year it is. and it depends
+            if the users already had their birthday.
+            Then it's only going to be right.
+            Some of the time.
+            So what we'll do is we'll check to see if theDateTime.AddYears(age)
+            is greater than date DateTime.Today.
+            And if that is the case then the user hasn't had their birthday yet this year.
+            So it will take a year off their age that we're returning above.
+            And then we'll return the age.
+        */
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            int age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+
+            return age;
+        }
     }
 }
