455b609021654bc949f1fec113292da498555b78
hassan adel <hasanadel555@hotmail.com>
2019-01-19 04:10:30 +0200

    Add UpdateUser to UserController

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using SocialApp.API.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SocialApp.API.Controllers
@@ -42,6 +43,26 @@ namespace SocialApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDTO>(user);
 
             return Ok(userToReturn);
+        }
+
+        // Put api/user/id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDTO userForUpdateDTO)
+        {
+            //check that the user that is attempting to update their profile matches
+            //the token the servers receiving.
+            //So what we'll do is compare the id of the path to the user's Id
+            //This being passed in as part of their token.
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+            _mapper.Map(userForUpdateDTO, userFromRepo);
+
+            if(await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on Save");
         }
 
     }
