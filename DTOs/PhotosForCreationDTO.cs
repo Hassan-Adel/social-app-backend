03346e39405dcbb2a3d307435fb8aba697a4cea4
hassan adel <hasanadel555@hotmail.com>
2019-01-20 02:38:44 +0200

    Add PhotosForCreationDTO

---
+++ DTOs/PhotosForCreationDTO.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.DTOs
+{
+    public class PhotosForCreationDTO
+    {
+        public string Url { get; set; }
+        public IFormFile File { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        // Cloudinary PublicId
+        public string PublicId { get; set; }
+        public PhotosForCreationDTO()
+        {
+            DateAdded = DateTime.Now;
+        }
+    }
+}
