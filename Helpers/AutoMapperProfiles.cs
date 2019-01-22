614664512bc6384ce24266918fff8ca3ffe563f0
hassan adel <hasanadel555@hotmail.com>
2019-01-22 04:17:36 +0200

    Add Mapper profile for UserForLocalStorageDTO

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -32,6 +32,17 @@ namespace SocialApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
 
+            // Same as UserForListDTO
+            CreateMap<User, UserForLocalStorageDTO>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                }).
+            ForMember(dest => dest.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+
             CreateMap<Photo, PhotosForDetailedDTO>();
             CreateMap<UserForUpdateDTO, User>();
             CreateMap<Photo, PhotosForReturnDTO>();
