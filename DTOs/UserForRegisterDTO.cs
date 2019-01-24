63d01a4371175dd54b54d0a0746fa32c629415f1
hassan adel <hasanadel555@hotmail.com>
2019-01-24 09:05:07 +0200

    Update UserForRegisterDTO

--- DTOs/UserForRegisterDTO.cs
+++ DTOs/UserForRegisterDTO.cs
@@ -14,5 +14,28 @@ namespace SocialApp.API.DTOs
         [Required]
         [StringLength(10, MinimumLength = 4, ErrorMessage = "Password should be between 4 and 10 characters.")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDTO()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
