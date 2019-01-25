0f58f763f255f783b373b0da0a901613af196a0d
hassan adel <hasanadel555@hotmail.com>
2019-01-25 02:52:30 +0200

    Adding Comments

--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -12,6 +12,9 @@ namespace SocialApp.API.Helpers
 
         private int pageSize = 10;
 
+        //if they request a page size of a thousand then the value is going to
+        //be greater than the max page size.
+        //So then we'll return a max page size which is 50.
         public int PageSize
         {
             get { return pageSize; }
