19d263250fae29f39161f7fd7efe3e446bfcf80c
hassan adel <hasanadel555@hotmail.com>
2019-01-25 02:25:51 +0200

    Add an ActionFilter (LogUserActivity) to update the user.LastActive whenever the user calls the UsersController

---
+++ Helpers/LogUserActivity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using SocialApp.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // await next() : wait  untill the action is completed
+            var resultContext = await next();
+            var userId = int.Parse(resultContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            //this is being provided as a service in our dependency injection container inside our StartUp class so we can get an instance of this service
+            //by using this and then we can say get service and we specify the service we want to get <ISocialRepository>.
+            var repo = resultContext.HttpContext.RequestServices.GetService<ISocialRepository>();
+
+            var user = await repo.GetUser(userId);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAll();
+        }
+    }
+}
