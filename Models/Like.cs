beb8d03dd1df08358c6e142020b4648a94021056
hassan adel <hasanadel555@hotmail.com>
2019-01-28 04:19:54 +0200

    Add Model "Like"

---
+++ Models/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Models
+{
+    public class Like
+    {
+        // Id of the user who likes
+        public int LikerId { get; set; }
+        // Id of the user who is liked
+        public int LikeeId { get; set; }
+        public User Liker { get; set; }
+        public User Likee { get; set; }
+    }
+}
