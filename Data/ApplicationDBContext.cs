7a899508478466213338a93450780e46aad3c3ae
hassan adel <hasanadel555@hotmail.com>
2019-01-28 04:33:59 +0200

    Add Like Entity
    EF Core <= 2.2 does not yet support many-to-many relationships without a join table.
    
    So we'll need to introduce a new table that relates to parent tables(User).

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -19,5 +19,24 @@ namespace SocialApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Composet key ( Also Meaning that the user cannot like the same user more than once)
+            builder.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict); // *When deleing the like the user will not be deleted
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict); // *When deleing the like the user will not be deleted
+        }
     }
 }
