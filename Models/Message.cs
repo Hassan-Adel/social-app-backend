c7295e93760c15a9935cea46fdc278526d958c87
hassan adel <hasanadel555@hotmail.com>
2019-01-30 18:22:43 +0200

    Add Message model

---
+++ Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+        public int RecipientId { get; set; }
+        public User Recipient { get; set; }
+    }
+}
