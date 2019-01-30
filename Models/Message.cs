0088cd76a64e722026ae6ef19133a74419ab847b
hassan adel <hasanadel555@hotmail.com>
2019-01-30 18:26:36 +0200

    Update Message model

--- Models/Message.cs
+++ Models/Message.cs
@@ -12,5 +12,11 @@ namespace SocialApp.API.Models
         public User Sender { get; set; }
         public int RecipientId { get; set; }
         public User Recipient { get; set; }
+        public string  Content { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; }
+        public bool SenderDeleted { get; set; }
+        public bool RecipientDeleted { get; set; }
     }
 }
