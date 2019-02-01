12ec3315680602c04683f5681c33606e2a81f206
hassan adel <hasanadel555@hotmail.com>
2019-02-01 03:49:40 +0200

    Adding MessageForCreationDTO

---
+++ DTOs/MessageForCreationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.DTOs
+{
+    public class MessageForCreationDTO
+    {
+        public int SenderId { get; set; }
+        public int RecipientId { get; set; }
+        public string Content { get; set; }
+        public DateTime MessageSent { get; set; }
+        public MessageForCreationDTO()
+        {
+            MessageSent = DateTime.Now;
+        }
+    }
+}
