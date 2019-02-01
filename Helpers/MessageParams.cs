34ab31d5af3fc5fc5fad9cb3c7144516d35ae07f
hassan adel <hasanadel555@hotmail.com>
2019-02-02 00:43:08 +0200

    Add MessageParams

---
+++ Helpers/MessageParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.Helpers
+{
+    public class MessageParams
+    {
+        private const int MaxPagSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int pageSize = 10;
+
+        //if they request a page size of a thousand then the value is going to
+        //be greater than the max page size.
+        //So then we'll return a max page size which is 50.
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPagSize) ? MaxPagSize : value; }
+        }
+        public int UserId { get; set; }
+        public string MessageCcontainer { get; set; } = "Unread";
+    }
+}
