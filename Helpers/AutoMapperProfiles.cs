4d3bb2a2f36380f3d87853ba463e82903b975aa1
hassan adel <hasanadel555@hotmail.com>
2019-02-02 01:12:33 +0200

    Update AutoMapperProfiles

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -49,6 +49,17 @@ namespace SocialApp.API.Helpers
             CreateMap<PhotosForCreationDTO, Photo>();
             CreateMap<UserForRegisterDTO, User>();
             CreateMap<MessageForCreationAndReturnDTO, Message>().ReverseMap();
+
+            CreateMap<Message, MessageToReturnDTO>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(p => p.IsMain).Url);
+                }).
+                ForMember(dest => dest.RecipientPhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url);
+                });
+
         }
     }
 }
