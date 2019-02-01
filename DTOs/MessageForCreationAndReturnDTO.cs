1db04dd43e05e38cd0d3604ad3cb76f297a11a5c
hassan adel <hasanadel555@hotmail.com>
2019-02-01 03:55:48 +0200

    renaming DTO to MessageForCreationAndReturnDTO

--- DTOs/MessageForCreationAndReturnDTO.cs
+++ DTOs/MessageForCreationAndReturnDTO.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace SocialApp.API.DTOs
 {
-    public class MessageForCreationDTO
+    public class MessageForCreationAndReturnDTO
     {
         public int SenderId { get; set; }
         public int RecipientId { get; set; }
         public string Content { get; set; }
         public DateTime MessageSent { get; set; }
-        public MessageForCreationDTO()
+        public MessageForCreationAndReturnDTO()
         {
             MessageSent = DateTime.Now;
         }
