b03072a7b6021ba241bcda5a3e39b5db50a4a8a5
hassan adel <hasanadel555@hotmail.com>
2019-02-01 03:51:01 +0200

    Adding GetMessage and CreateMessage to MessagesController

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -27,7 +27,45 @@ namespace SocialApp.API.Controllers
             _repo = repo;
             _mapper = mapper;
         }
-
         
+        // GET /id
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int id)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok(messageFromRepo);
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDTO messageForCreationDTO)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageForCreationDTO.SenderId = userId;
+            var reccipient = await _repo.GetUser(messageForCreationDTO.RecipientId);
+
+            if (reccipient == null)
+                BadRequest("Could not find user");
+
+            var message = _mapper.Map<Message>(messageForCreationDTO);
+
+            _repo.Add(message);
+
+            if (await _repo.SaveAll())
+                return CreatedAtRoute("GetMessage", new { id = message.Id }, message);
+
+            throw new Exception("Creating the message failedon save");
+
+        }
+
     }
 }
