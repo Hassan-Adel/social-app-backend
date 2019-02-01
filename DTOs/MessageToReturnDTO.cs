fcd0725475c07fd270806ff80e29325e7009bf8f
hassan adel <hasanadel555@hotmail.com>
2019-02-02 01:12:17 +0200

    Add MessageToReturnDTO

---
+++ DTOs/MessageToReturnDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.API.DTOs
+{
+    public class MessageToReturnDTO
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        //Even though this isn't just known as, because we've got a property
+        //of known as in our User class then auto mapper should be able to figure it out.
+        //And return the sender known as as the users known as property.
+        public string SenderKnownAs { get; set; }
+        public string SenderPhotoUrl { get; set; }
+        public int RecipientId { get; set; }
+        public string RecipientKnownAs { get; set; }
+        public string RecipientPhotoUrl { get; set; }
+        public string Content { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; }
+    }
+}
