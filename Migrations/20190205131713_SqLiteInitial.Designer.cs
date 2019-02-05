9e6589ae215a9a3f353a3d7aee70e09e65a41154
hassan adel <hasanadel555@hotmail.com>
2019-02-05 23:03:45 +0200

    Removing MySql migrations and adding SqLite

--- Migrations/20190205131713_SqLiteInitial.Designer.cs
+++ Migrations/20190205131713_SqLiteInitial.Designer.cs
@@ -9,15 +9,14 @@ using SocialApp.API.Data;
 namespace SocialApp.API.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    [Migration("20190204163930_MySqlInitialCreate")]
-    partial class MySqlInitialCreate
+    [Migration("20190205131713_SqLiteInitial")]
+    partial class SqLiteInitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
-                .HasAnnotation("Relational:MaxIdentifierLength", 64);
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024");
 
             modelBuilder.Entity("SocialApp.API.Models.Like", b =>
                 {
