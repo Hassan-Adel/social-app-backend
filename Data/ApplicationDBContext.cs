782277b0f73ee5c446f0a1604922ecb29553c247
hassan adel <hasanadel555@hotmail.com>
2019-02-06 16:59:51 +0200

    Configuring the DataContext for Identity

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SocialApp.API.Models;
 using System;
 using System.Collections.Generic;
@@ -13,17 +15,39 @@ namespace SocialApp.API.Data
      * 2- Update-Database
      * 3- Remove-Migration (to remove latest migration)
      */
-    public class ApplicationDBContext : DbContext
+    //This looks a bit weird but the reason for doing this is purely so I can use.
+    //Ids everywhere inside's my database rather than using strings (which is the default).
+    public class ApplicationDBContext : IdentityDbContext <User, Role, int, IdentityUserClaim<int>, UserRole,
+                                        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base (options) { }
         public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
+            //So entity framework knows about the relationship between
+            //our users and our roles and the user role because a user can be part
+            //of many roles and the role can be associated with many users (many to many relation).
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+            });
+
             // Composet key ( Also Meaning that the user cannot like the same user more than once)
             builder.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
 
