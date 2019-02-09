241dc65b15fab69cca60b34b61b3313fb723d7a9
hassan adel <hasanadel555@hotmail.com>
2019-02-09 02:00:34 +0200

    Remove old migrations and Add "SqLiteIdentityInitial" with the ASP Identity tables added

--- Migrations/20190208235839_SqLiteIdentityInitial.Designer.cs
+++ Migrations/20190208235839_SqLiteIdentityInitial.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialApp.API.Data;
 
 namespace SocialApp.API.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190208235839_SqLiteIdentityInitial")]
+    partial class SqLiteIdentityInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
