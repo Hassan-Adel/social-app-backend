5f7ce7cbb78bdeb6e1e584d47694948859b2fc3e
hassan adel <hasanadel555@hotmail.com>
2019-02-11 23:45:29 +0200

    Create AdminController

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SocialApp.API.Data;
+using SocialApp.API.DTOs;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+
+        public AdminController (UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration config, IMapper mapper)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+            _mapper = mapper;
+        }
+
+        // GET
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("usersWithRoles")]
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            return Ok("Only for admins");
+        }
+
+        // GET
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photosForModeration")]
+        public async Task<IActionResult> GetPhotosForModeration()
+        {
+            return Ok("Allowed for admins and moderators");
+        }
+    }
+}
